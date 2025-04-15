a4d972bae082dee759d53e781c2b6afe260d6993
RahulGupta77 <rg3498104@gmail.com>
2025-04-15 16:53:06 +0530

    Changed the chat with ai api url

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -2,7 +2,7 @@ import apiClient from "./apiClient";
 
 export const send_message_to_llm = async (user_uuid, message) => {
   try {
-    const response = await apiClient.post("/chat_with_ai", {
+    const response = await apiClient.post("/messages", {
       user_uuid,
       message,
     });
@@ -33,9 +33,9 @@ export const send_file_to_llm = async (user_uuid, file) => {
 
 export const get_user_chat_history = async (user_uuid) => {
   try {
-    const response = await apiClient.post("/chat_history", {
-      user_uuid,
-    });
+    const response = await apiClient.get(
+      `/messages?user_uuid=${encodeURIComponent(user_uuid)}`
+    );
     return response;
   } catch (error) {
     console.error(error.response.data.msg);
